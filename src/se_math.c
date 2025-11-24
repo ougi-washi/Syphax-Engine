09121257025ad14cb627d397a17ec87481608691
ougi-washi <jedfakhfekh@gmail.com>
2025-11-24 21:10:17 +0900

    Fix math 2d box and 3d box intersection implementation

--- src/se_math.c
+++ src/se_math.c
@@ -110,3 +110,29 @@ se_mat4 mat4_scale(const se_vec3* v) {
     return S;
 }
 
+b8 se_box_2d_is_inside(const se_box_2d* a, const se_vec2* p) {
+    return p->x >= a->min.x && p->x <= a->max.x &&
+        p->y >= a->min.y && p->y <= a->max.y;
+}
+
+b8 se_box_2d_intersects(const se_box_2d* a, const se_box_2d* b) {
+    return a->min.x <= b->max.x && a->max.x >= b->min.x &&
+        a->min.y <= b->max.y && a->max.y >= b->min.y;
+}
+
+b8 se_box_3d_intersects(const se_box_3d* a, const se_box_3d* b) {
+    return a->min.x <= b->max.x && a->max.x >= b->min.x &&
+        a->min.y <= b->max.y && a->max.y >= b->min.y &&
+        a->min.z <= b->max.z && a->max.z >= b->min.z;
+}
+
+b8 se_circle_intersects(const se_circle* a, const se_circle* b) {
+    return fabs(a->position.x - b->position.x) <= a->radius + b->radius ||
+        fabs(a->position.y - b->position.y) <= a->radius + b->radius;
+}
+
+b8 se_sphere_intersects(const se_sphere* a, const se_sphere* b) {
+    return fabs(a->position.x - b->position.x) <= a->radius + b->radius ||
+        fabs(a->position.y - b->position.y) <= a->radius + b->radius ||
+        fabs(a->position.z - b->position.z) <= a->radius + b->radius;
+}
--- src/se_math.h
+++ src/se_math.h
@@ -12,6 +12,14 @@ typedef struct { f32 x, y, z; } se_vec3;
 typedef struct { f32 x, y, z, w; } se_vec4;
 typedef struct { f32 m[16]; } se_mat4;
 
+// 2D
+typedef struct { se_vec2 min, max; } se_box_2d;
+typedef struct { se_vec2 position; f32 radius; } se_circle;
+
+// 3D
+typedef struct { se_vec3 min, max; } se_box_3d;
+typedef struct { se_vec3 position; f32 radius; } se_sphere;
+
 #define se_vec(_vec_size, ...) ( se_vec##_vec_size ) { __VA_ARGS__ }
 #define se_vec2(_x, _y) (se_vec(2, _x, _y))
 #define se_vec3(_x, _y, _z) (se_vec(3, _x, _y, _z))
@@ -31,17 +39,23 @@ static se_mat4 mat4_identity(void) {
 #define PI 3.14159265359
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
-f32 vec3_length(se_vec3 v);
-se_mat4 mat4_perspective(float fov_y, float aspect, float near, float far);
-se_vec3 vec3_sub(se_vec3 a, se_vec3 b);
-se_vec3 vec3_norm(se_vec3 v);
-se_vec3 vec3_cross(se_vec3 a, se_vec3 b);
-se_mat4 mat4_look_at(se_vec3 eye, se_vec3 center, se_vec3 up);
-se_mat4 mat4_mul(const se_mat4 A, const se_mat4 B);
-se_mat4 mat4_translate(const se_vec3* v); 
-se_mat4 mat4_rotate_x(se_mat4 m, f32 angle);
-se_mat4 mat4_rotate_y(se_mat4 m, f32 angle);
-se_mat4 mat4_rotate_z(se_mat4 m, f32 angle);
-se_mat4 mat4_scale(const se_vec3* v);
+extern f32 vec3_length(se_vec3 v);
+extern se_mat4 mat4_perspective(float fov_y, float aspect, float near, float far);
+extern se_vec3 vec3_sub(se_vec3 a, se_vec3 b);
+extern se_vec3 vec3_norm(se_vec3 v);
+extern se_vec3 vec3_cross(se_vec3 a, se_vec3 b);
+extern se_mat4 mat4_look_at(se_vec3 eye, se_vec3 center, se_vec3 up);
+extern se_mat4 mat4_mul(const se_mat4 A, const se_mat4 B);
+extern se_mat4 mat4_translate(const se_vec3* v); 
+extern se_mat4 mat4_rotate_x(se_mat4 m, f32 angle);
+extern se_mat4 mat4_rotate_y(se_mat4 m, f32 angle);
+extern se_mat4 mat4_rotate_z(se_mat4 m, f32 angle);
+extern se_mat4 mat4_scale(const se_vec3* v);
+
+extern b8 se_box_2d_is_inside(const se_box_2d* a, const se_vec2* p);
+extern b8 se_box_2d_intersects(const se_box_2d* a, const se_box_2d* b);
+extern b8 se_box_3d_intersects(const se_box_3d* a, const se_box_3d* b);
+extern b8 se_circle_intersects(const se_circle* a, const se_circle* b);
+extern b8 se_sphere_intersects(const se_sphere* a, const se_sphere* b);
 
 #endif // SE_MATH_H
